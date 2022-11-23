6abbac415ea3d2a4a5a6081f617aa1d78f65641e
Victor Zuniga <vzuniga@poudrelibraries.org>
2022-11-23 13:25:51 -0700

    Update Delete-archived-files-older-than-7-days.bat
    Cleanup of the file paths on the script.

--- scripts/Delete-archived-files-older-than-7-days.bat
+++ scripts/Delete-archived-files-older-than-7-days.bat
@@ -6,10 +6,9 @@ echo.
 echo Removing archived files older than 7 days...
 echo.
 echo.
-forfiles /p "C:\Shoutbomb\Archive\Holds" /s /m *_MDT.txt /D -7 /C "cmd /c del /F /Q @path"
-forfiles /p "C:\Shoutbomb\Archive\Renewals" /s /m *_MDT.txt /D -7 /C "cmd /c del /F /Q @path"
-forfiles /p "C:\Shoutbomb\Archive\Overdue" /s /m *_MDT.txt /D -7 /C "cmd /c del /F /Q @path"
+forfiles /p "C:\Orangeboy\Archive\Circulation" /s /m prpld_circ_*.txt /D -7 /C "cmd /c del /F /Q @path"
+forfiles /p "C:\Orangboy\Archive\EBSCO" /s /m PoudreLibraries_isbn_*.txt /D -7 /C "cmd /c del /F /Q @path"
+forfiles /p "C:\Orangboy\Archive\WAM" /s /m prpld_database_*.txt /D -7 /C "cmd /c del /F /Q @path"
 
-REM Close the FTP connection to the Shoutbomb server
 
 quit
